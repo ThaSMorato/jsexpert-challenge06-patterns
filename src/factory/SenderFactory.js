dd3b1743995c48b80f32dffa8e797c8c18625902
ThaSMorato <tha.s.morato@gmail.com>
2022-09-09 13:38:05 -0300

    refact: sender factory

--- src/factory/SenderFactory.js
+++ src/factory/SenderFactory.js
@@ -2,8 +2,11 @@ import CreditsRepository from "../db/CreditsRepository";
 
 export class SenderFactory {
   static async createSenderInstance(platform) {
+    const service = {
+      send: console.log,
+    };
     const credits_repository = new CreditsRepository({ db: {} });
     const { default: Sender } = await import(`../modules/message/sender/${platform}/Sender`);
-    return new Sender({ credits_repository });
+    return new Sender({ service, credits_repository });
   }
 }
